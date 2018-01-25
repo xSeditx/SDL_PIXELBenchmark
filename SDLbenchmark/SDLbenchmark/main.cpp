338dd30aa944043b4e6d8f85ef7998a5c13b9fae
xSeditx <33107273+xSeditx@users.noreply.github.com>
2018-01-25 03:04:00 -0500

    Update main.cpp

--- SDLbenchmark/SDLbenchmark/main.cpp
+++ SDLbenchmark/SDLbenchmark/main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 #include"Window.h"
 #include <memory>
+
+// Just realized after loading this to Github, Your going to need your SDL2.lib & SDLmain.lib like normal linked, SDL_IMAGE...maybe,
+// I dont think any of those functions are here but I made this from another project I was working on so idk what the linker thinks 
+// about that
+//
+
 using namespace std;
 
 
@@ -9,21 +15,18 @@ void main()
 
 	WINDOW Main(0,0,300,300,"SDL Benchmark Test");
 	SET_ACTIVE_WINDOW(&Main);
-	
-	
+
 	while(LOOP_GAME()){
-	_CLS;
-	double TIME = SDL_GetTicks();
-	        ITERATE(y,SCREEN->HEIGHT){
-	        	ITERATE(x,SCREEN->WIDTH){
+_CLS;
+	        ITERATE(y, SCREEN->HEIGHT){
+	        	ITERATE(x, SCREEN->WIDTH){
 					    Uint32 COLOR = RGB(RANDOM(255),RANDOM(255),RANDOM(255));
 					    SET_PIXEL(x,y,COLOR);
 	        	}
 	        }
-			Print(SCREEN->FPS); TIME = 0;
-    _SYNC;
+			Print(SCREEN->FPS);
+_SYNC;
 	}
-
 
 }
 
@@ -34,4 +37,5 @@ int KEYBOARD_HANDLER(int);
 int KEYBOARD_HANDLER(int message){
 	return false;
 }
-//================================= Just added it because it was screaming at me because I didnt make one like I normally do lol
+//======Just added it because compiler was screaming at me because I didnt make one like I normally do lol and my little window abstraction
+// kind of requires that I have a input handler
